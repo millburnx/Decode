7eb5f0d31948ca10385512ec3c7313df755806b8
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-11-11 03:16:02 -0500

    Axis Snapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Pedro.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystem/Pedro.kt
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.common.subsystem
 
+import com.bylazar.configurables.annotations.Configurable
 import com.millburnx.cmdx.Command
 import com.millburnx.cmdxpedro.util.Pose2d
 import com.millburnx.cmdxpedro.util.WaitFor
@@ -7,8 +8,10 @@ import com.millburnx.cmdxpedro.util.toDegrees
 import com.pedropathing.geometry.Pose
 import org.firstinspires.ftc.teamcode.opmode.OpMode
 import org.firstinspires.ftc.teamcode.pedro.Constants
+import kotlin.math.abs
+import kotlin.math.pow
 
-
+@Configurable
 class Pedro(opMode: OpMode, var isTeleop: Boolean = false) : Subsystem("Pedro") {
     val follower = Constants.createFollower(opMode.hardwareMap).apply {
         setStartingPose(Pose(0.0, 0.0, 0.0))
@@ -26,14 +29,28 @@ class Pedro(opMode: OpMode, var isTeleop: Boolean = false) : Subsystem("Pedro") 
             while (!isStopRequested) {
                 follower.update()
                 if (isTeleop && !isLocked) {
+                    // axis snapping
+                    val snappedX = snapTo(gp1.current.leftJoyStick.x, 0.0, snapThreshold)
+                    val snappedY = snapTo(gp1.current.leftJoyStick.y, 0.0, snapThreshold)
+
                     follower.setTeleOpDrive(
-                        -gp1.current.leftJoyStick.y,
-                        -gp1.current.leftJoyStick.x,
-                        -gp1.current.rightJoyStick.x
+                        -snappedX.pow(3),
+                        -snappedY.pow(3),
+                        -gp1.current.rightJoyStick.x.pow(3)
                     )
                 }
                 sync()
             }
         }
+    }
+
+    private fun snapTo(raw: Double, target: Double, threshold: Double): Double {
+        if (abs(raw - target) < threshold) return target
+        return raw
+    }
+
+    companion object {
+        @JvmField
+        var snapThreshold = 0.1;
     }
 }
