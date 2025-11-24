7305752d0297459a3a07035eafa6dc5b211eb3cf
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-11-23 22:18:37 -0500

    random ll stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util.kt
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode.common
 
 import com.millburnx.cmdxpedro.util.Pose2d
 import com.millburnx.cmdxpedro.util.geometry.vector.Vec2d
+import com.millburnx.cmdxpedro.util.toDegrees
+import com.pedropathing.ftc.FTCCoordinates
+import com.pedropathing.geometry.PedroCoordinates
 import com.pedropathing.geometry.Pose
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D
 import java.util.*
 import kotlin.math.atan2
 import kotlin.math.cos
@@ -44,4 +50,13 @@ fun Double.fieldMirror(): Double {
 fun Pose2d.remainingAngleTo(other: Vec2d): Double {
     val angleTo = angleTo(other)
     return normalizeRadians(angleTo - radians)
+}
+
+fun Pose3D.toPose(): Pose2d {
+    val pos = position.toUnit(DistanceUnit.INCH)
+    val heading = orientation.getYaw(AngleUnit.RADIANS)
+//    val base = Pose(pos.x, pos.y, heading, FTCCoordinates.INSTANCE)
+//    val converted = base.getAsCoordinateSystem(PedroCoordinates.INSTANCE)
+//    return Pose2d(converted.x, converted.y, converted.heading.toDegrees())
+    return Pose2d(pos.x, pos.y, heading)
 }
