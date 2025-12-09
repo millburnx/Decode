5e253da786c6cb05916204c6d260ead9c1b80e45
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-12-09 14:36:15 -0500

    Auton Manager w/ file based path loading

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auton/AutonManager.kt
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.opmode.auton
+
+
+import com.millburnx.cmdxpedro.paths.PedroBuilder
+import com.millburnx.cmdxpedro.paths.PedroLoader
+import com.millburnx.pedroparser.types.Path
+import com.millburnx.pedroparser.types.Sequence
+import com.millburnx.util.Pose2d
+
+// TODO: create the pedro subsystem and pass it so we can automatically the starting pose
+//  (find a way to do it cleanly since this is created after it)
+public class AutonManager(public val isMirrored: Boolean = false, public val sequenceName: String) {
+    public val builder: PedroBuilder = PedroBuilder(isMirrored)
+    public val loadedSequence: Sequence = PedroLoader.load(sequenceName)
+    public val loadedPaths: List<Path>
+        get() = loadedSequence.lines
+    public val loadedPathsMap: Map<String, Path>
+        get() = loadedPaths.associateBy { it.name }
+    public val startingPose: Pose2d
+        get() = loadedSequence.startPose
+}
